a4c491f5a21616a68a2c3989cbd62f21d04ace66
Samyak221 <114600077+Samyak221@users.noreply.github.com>
2023-10-18 21:19:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js'
 import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config()
 
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
